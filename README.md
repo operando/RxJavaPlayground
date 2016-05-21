05af73f6033df4af864315ebea27da8c2a893c3e
operando <okanoshinobu8@gmail.com>
2016-05-22 00:28:20 +0900

    add sample error handling

--- README.md
+++ README.md
@@ -8,6 +8,10 @@
 * [RxJavaについて調べた試した](http://kirimin.hatenablog.com/entry/20141012/1413126770)
 * [RxJava学習のベストプラクティスっぽいもの by yagi](https://speakerdeck.com/sys1yagi/rxjavaxue-xi-falsehesutohurakuteisutuhoimofalse)
 
+### エラーハンドリング
+
+* [RxJavaのエラーハンドリング](http://qiita.com/boohbah/items/108b378c5cb593c666e6)
+
 ## その他
 
 どうやってObservable化するか
--- src/main/java/rx/playground/Main.java
+++ src/main/java/rx/playground/Main.java
@@ -5,7 +5,9 @@ import rx.Observer;
 import rx.Subscriber;
 import rx.functions.Action1;
 import rx.functions.Func1;
+import rx.functions.Func2;
 
+import java.net.SocketException;
 import java.util.concurrent.TimeUnit;
 
 public class Main {
@@ -90,5 +92,65 @@ public class Main {
                 })
                 .skip(2)
                 .subscribe(PrintObserver.create());  // 2 - 4の値が来る
+
+        Observable
+                .create((Observable.OnSubscribe<String>) subscriber -> {
+                    subscriber.onNext("test");
+                    subscriber.onError(new Exception());
+                })
+                .onErrorReturn(throwable -> "error")
+                .subscribe(PrintObserver.create());
+
+        Observable
+                .create((Observable.OnSubscribe<Integer>) subscriber -> {
+                    subscriber.onError(new Exception());
+                })
+                .onErrorResumeNext(new Func1<Throwable, Observable<? extends Integer>>() {
+                    @Override
+                    public Observable<? extends Integer> call(Throwable throwable) {
+                        return Observable.just(-1);
+                    }
+                })
+                .subscribe(PrintObserver.create());
+
+        Observable
+                .create((Observable.OnSubscribe<String>) subscriber -> {
+                    System.out.println("Retry Test");
+                    subscriber.onNext("test");
+                    subscriber.onError(new Exception());
+                })
+                .retry(2)
+                .onErrorReturn(throwable -> "error")
+//                .retry(2) // こっちに書くと効果はない
+                .subscribe(PrintObserver.create());
+
+        Observable
+                .create(new Observable.OnSubscribe<String>() {
+                    int count;
+
+                    @Override
+                    public void call(Subscriber<? super String> subscriber) {
+                        System.out.println(count);
+//                        if (count > 2) {
+                        if (count > 1) {
+                            subscriber.onError(new SocketException());
+                        } else {
+                            subscriber.onError(new Exception());
+                        }
+                        count++;
+                    }
+                })
+                .retry(new Func2<Integer, Throwable, Boolean>() {
+                    @Override
+                    public Boolean call(Integer count, Throwable throwable) {
+                        // SocketExceptionが起きたらError
+                        if (throwable instanceof SocketException) {
+                            return false;
+                        }
+                        // ２回まではリトライする
+                        return count < 3;
+                    }
+                })
+                .subscribe(PrintObserver.create());
     }
 }
